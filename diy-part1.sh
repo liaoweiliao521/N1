e1141a2e0ff9c80efbf892a6a0c087931f4881b3
liaoweiliao521 <liaoweiliao521@gmail.com>
2021-08-24 09:19:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,4 +12,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/lxhao61/openwrt-passwall.git' >>feeds.conf.default
